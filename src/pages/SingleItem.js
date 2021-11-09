de1a4ac05a07c602664beeb4f9cbec351303033a
Henry WL <henrywl@gmail.com>
2021-11-09 23:59:09 +0000

    Styling

--- src/pages/SingleItem.js
+++ src/pages/SingleItem.js
@@ -8,14 +8,14 @@ const SingleItem = () => {
   const { id } = useParams();
   return (
     <div className="singleItem">
-      <h1>Single Item Page</h1>
-      <h1>{data.items[id].name}</h1>
-      <h1>{data.items[id].price}</h1>
+      {/* <h1>Single Item Page</h1> */}
+      <h1 className="singleItemH1">{data.items[id].name}</h1>
+      <h1 className="singleItemH1">{data.items[id].price}</h1>
       <img src={data.items[id].image} alt={data.items[id].name}></img>
 
       {/* <h1>{data.items[id].id}</h1> */}
       <br></br>
-      <button onClick={() => data.addItemToCart(data.items[id])}>Add to Cart</button>
+      <button className="itemAddBtn" onClick={() => data.addItemToCart(data.items[id])}>Add to Cart</button>
     </div>
   );
 };
